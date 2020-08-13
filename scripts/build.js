e77868cb29d42317f4c71f5197d2bbe664ce7751
hzmming <844155285@qq.com>
2020-08-13 15:08:07 +0800

    build输出错误信息

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,23 @@ const webpack = require("webpack"),
 
 delete config.chromeExtensionBoilerplate;
 
-webpack(config, function (err) {
-  if (err) throw err;
+webpack(config, function (err, stats) {
+  // https://webpack.js.org/api/node/#error-handling
+  if (err) {
+    console.error(err.stack || err);
+    if (err.details) {
+      console.error(err.details);
+    }
+    return;
+  }
+
+  const info = stats.toJson();
+
+  if (stats.hasErrors()) {
+    console.error(info.errors);
+  }
+
+  if (stats.hasWarnings()) {
+    console.warn(info.warnings);
+  }
 });
