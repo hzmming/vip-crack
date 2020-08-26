180f5e462192fb8927a470ada86f9cb3d2fd1e8b
LoryHuang <844155285@qq.com>
2020-08-26 19:38:54 +0800

    build: 将husky和lint-staged提取至root

---
+++ .lintstagedrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "packages/jsonp-hook/src/**/*.{js,vue}": ["eslint --fix"],
+  "packages/vip-crack-core/src/**/*.{js,vue}": ["eslint --fix"],
+  "packages/vip-crack-extension/src/**/*.{js,vue}": ["eslint --fix"],
+};
