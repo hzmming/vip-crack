3dfe2b26fd946dc522868cca679c19e60f56d235
LoryHuang <844155285@qq.com>
2020-09-01 02:14:11 +0800

    feat: 调整api测试用例

--- packages/vip-crack-extension/test/api/api.spec.js
+++ packages/vip-crack-extension/test/api/api.spec.js
@@ -1,22 +1,15 @@
 const { list: apiList } = require("../../src/apiList.json");
-const { generateRequestParams } = require("shared/util");
 const fetch = require("node-fetch");
 
-// 《命运石之门》第2集
-const videoUrl = "https://v.qq.com/x/cover/gvz5bapszkkjbw8/b0026nzjshz.html";
+jest.setTimeout(15 * 1000);
 
 describe("api", () => {
   it("should request successful", async () => {
     for (let i = 0; i < apiList.length; i++) {
-      const { url, options } = generateRequestParams(videoUrl, apiList[i].api);
-      const res = await fetch(url, options);
-      let data = null;
-      try {
-        data = await res.json();
-      } catch (e) {
-        console.error(e);
-      }
-      expect(data && data.success).toBeTruthy();
+      const result = await fetch(apiList[i]);
+      console.log(result.ok, apiList[i]);
+      // 保证可达即可
+      expect(result.ok).toBeTruthy();
     }
   });
 });
