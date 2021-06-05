149ac3765b51f75dc61ba5ad9810b41528fd6745
LoryHuang <844155285@qq.com>
2021-06-05 16:01:26 +0800

    feat: 去除不可用接口及调整下测试用例

--- packages/vip-crack-extension/test/api/api.spec.js
+++ packages/vip-crack-extension/test/api/api.spec.js
@@ -5,11 +5,25 @@ jest.setTimeout(15 * 1000);
 
 describe("api", () => {
   it("should request successful", async () => {
+    const errorList = [];
+    const successList = [];
     for (let i = 0; i < apiList.length; i++) {
-      const result = await fetch(apiList[i]);
-      console.log(result.ok, apiList[i]);
-      // 保证可达即可
-      expect(result.ok).toBeTruthy();
+      const url = apiList[i];
+      const result = await fetch(url).catch(() => {
+        errorList.push(url);
+      });
+      if (!result) continue;
+      if (result.ok) {
+        successList.push(url);
+      } else {
+        errorList.push(url);
+      }
     }
+    // 保证可达即可
+    errorList.length &&
+      console.log("以下api均已不可用：\n", errorList.join("\n"));
+
+    console.log("可用api如下：\n", successList.join("\n"));
+    expect(errorList.length).toBe(0);
   });
 });
